030008cc524bcba31cd239c4f671323419ec9487
rippera <mikheil.mezurnishvili10b@geolab.edu.ge>
2019-05-24 16:05:40 +0400

    rippera

--- js/script.js
+++ js/script.js
@@ -1,46 +1,57 @@
+// global variables
 const liItems = [...document.querySelectorAll('.drop')];
 const notficationsOut = document.querySelector('.notfications_out');
 const notficati = document.querySelectorAll('.notficati .drop_li').length;
 const labels = document.querySelectorAll('.turn_labels');
 const onOff = document.querySelectorAll('.on_off');
+const burger = document.querySelector('.burger');
+
+
 notficationsOut.textContent = notficati;
+// funtions
 
 function leftNavClick(item) {
-    item.addEventListener('click',(e)=>{
+    item.addEventListener('click', (e) => {
         const ul = document.querySelector('.drop_ul')
         const next = e.target.nextElementSibling;
         if (!e.target.lastElementChild.classList.contains('active')) {
             e.target.lastElementChild.classList.add('active');
-        }else{
+        } else {
             e.target.lastElementChild.classList.remove('active');
         }
     });
 }
 $(function () {
     $('#datetimepicker1').datetimepicker({
-        format:'YYYY-MM-DD',
+        format: 'YYYY-MM-DD',
         useCurrent: false
     });
 });
 
 function changeLabels(item) {
-    item.addEventListener('click',(e)=>{
+    item.addEventListener('click', (e) => {
         if (e.target.classList.contains('off')) {
             e.target.classList.remove('off');
             e.target.previousSibling.previousSibling.classList.remove('off');
-        }else{
+        } else {
             e.target.classList.add('off');
             e.target.previousSibling.previousSibling.classList.add('off');
         }
     });
 }
 
+// eventlisteners 
 
-labels.forEach(item=>{
+burger.addEventListener('click', () => {
+    const menu = document.querySelector('.fixed');
+    const sss = document.querySelector('.burger span');
+    sss.classList.toggle('toggler');
+    menu.classList.toggle('open');
+})
+
+labels.forEach(item => {
     changeLabels(item);
 })
-liItems.forEach(item=>{
+liItems.forEach(item => {
     leftNavClick(item);
 });
-
-
